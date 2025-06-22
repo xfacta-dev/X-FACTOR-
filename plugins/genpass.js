b17e0bd438e4d92c5df35d4967aa4c6fd518d642
xfacta-dev <ronaldoderoojunge@gmail.com>
2025-06-22 13:09:24 +0300

    Update genpass.js

--- plugins/genpass.js
+++ plugins/genpass.js
@@ -56,7 +56,7 @@ cmd({
 
     // Send the message with the generated password
     await conn.sendMessage(from, {
-      text: "🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n" + generatedPassword + "\n\n*Powered By Popkid*"
+      text: "🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n" + generatedPassword + "\n\n*Powered By xfacta*"
     }, {
       quoted: quoted
     });
