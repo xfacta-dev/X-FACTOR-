351d68a8547c111af3e63dd63ad9b1f1635c912a
xfacta-dev <ronaldoderoojunge@gmail.com>
2025-06-22 13:45:19 +0300

    Update mainmenu.js

--- plugins/mainmenu.js
+++ plugins/mainmenu.js
@@ -326,7 +326,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '120363354023106228@newsletter',
-                        newsletterName: 'Popkiddevs',
+                        newsletterName: 'xfacta dev',
                         serverMessageId: 143
                     }
                 }
